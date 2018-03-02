32fbd8ff604c36678a53fa0f58d9a0d258927d88
Guillaume <guillaume.gobert@gmail.com>
2018-03-02 11:41:23 +0100

    Ajout de la librairie JFiglet et mise en place

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,8 @@
 package dev;
 
+//import
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+        String asciiArt = FigletFont.convertOneLine("App");
+        System.out.println(asciiArt);
     }
 }
