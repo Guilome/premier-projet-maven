a98deb82a335c102f4ea8e4da8e86fd1c54389cf
Guillaume <guillaume.gobert@gmail.com>
2018-03-02 16:51:03 +0100

    ajout des profils

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -2,6 +2,7 @@ package dev;
 
 //import
 import com.github.lalyos.jfiglet.FigletFont;
+import java.util.ResourceBundle;
 
 /**
  * Hello world!
@@ -11,8 +12,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        //System.out.println( "Hello World!" );
-        String asciiArt = FigletFont.convertOneLine("L'application    la    plus    cool");
-        System.out.println(asciiArt);
+    	//Affichage avec Figlet    	
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
+    	System.out.println(asciiArt);
+    	//Affichage de l'environnement
+    	String environnement = ResourceBundle.getBundle("application").getString("environnement");
+    	System.out.println("Environnement : " + environnement);
     }
 }
